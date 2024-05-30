0592b31bd80a44a0fc3d98abc8950dd6c310d18e
AlessandriaElia <e.alessandria.2616@vallauri.edu>
2024-05-30 20:34:43 +0200

    Updated style and improved form selection inputs

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
-"use strict"
+"use strict";
 $(document).ready(function() {
 
     const selectCitta = $('.form-control[required]');
     const btnCerca = $("#btnCerca");
+    const $checkInDate = $("#check-in");
+    const $checkOutDate =  $("#check-out");
+    const $adults = $('input[type="number"][placeholder="Adulti"]');
+    const $children = $('input[type="number"][placeholder="Bambini"]');
+
+    const checkInPicker = flatpickr("#check-in", {
+        dateFormat: "Y-m-d",
+        minDate: "today",
+        onChange: function(selectedDates) {
+            const [selectedDate] = selectedDates;
+            if (selectedDate) {
+                const dayAfterSelectedDate = new Date(selectedDate.getTime() + 86400000); 
+                checkOutPicker.set("minDate", dayAfterSelectedDate);
+                if (checkOutPicker.selectedDates[0] < dayAfterSelectedDate) {
+                    checkOutPicker.setDate(dayAfterSelectedDate); 
+                }
+            }
+        }
+    });
+    
+    const checkOutPicker = flatpickr("#check-out", {
+        dateFormat: "Y-m-d",
+        minDate: new Date().fp_incr(1) 
+    });
+    
 
     let rq = inviaRichiesta("GET", "server/getCitta.php");
     rq.catch(errore);
@@ -12,11 +37,13 @@ $(document).ready(function() {
             const option = $("<option>").val(citta["id"]).text(citta["citta"]);
             option.appendTo(selectCitta);
         });
-    })
+    });
 
     btnCerca.on("click", cercaHotel);
 
-    function cercaHotel(){
+    function cercaHotel(event){
+        event.preventDefault(); 
+
         const citta = selectCitta.val();
         const checkIn = $checkInDate.val();
         const checkOut = $checkOutDate.val();
@@ -26,4 +53,4 @@ $(document).ready(function() {
         console.log(citta, checkIn, checkOut, adults, children);
     }
 
-})
+});
